23f4222e1a8be41a0498b5ffd62253b2c357590a
gorkhiyamonika <gorkhiyamonika@gmail.com>
2024-12-10 16:26:21 +0530

    Step2 : configure svelte with astro

--- webapp/astro.config.mjs
+++ webapp/astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://improwised.github.io',
-    base: 'improwised-new-website',
-  });
+  site: 'https://improwised.github.io',
+  base: 'improwised-new-website',
+  integrations: [svelte()]
+});
