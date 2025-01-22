9fc9d0a39091080d7d68f33ba0305064be4f87d0
gorkhiyamonika <gorkhiyamonika@gmail.com>
2025-01-22 18:09:29 +0530

    feat: path change

--- newapp/astro.config.mjs
+++ newapp/astro.config.mjs
@@ -3,4 +3,4 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({site: 'https://improwised.github.io',
-    base: 'improwised-new-website/newapp',});
+    base: 'improwised-new-website-newapp',});
