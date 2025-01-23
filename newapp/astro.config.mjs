ee8cd34a47ea2bde770ccd67b5358dea2beb06d5
Monika Gorkhiya <97655102+monikagorkhiya@users.noreply.github.com>
2025-01-23 14:13:02 +0530

    Update astro.config.mjs
    Signed-off-by: Monika Gorkhiya <97655102+monikagorkhiya@users.noreply.github.com>

--- newapp/astro.config.mjs
+++ newapp/astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://improwised.github.io',
-    base: 'improwised-new-website',
-    outDir: './newapp'
+    base: '/improwised-new-website/newapp', // Subdirectory path
+    outDir: 'dist', // Default output directory
 });
