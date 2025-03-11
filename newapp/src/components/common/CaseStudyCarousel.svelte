9468585f1e5098ea6f734b0dca9133d1b57ffbf7
gorkhiyamonika <gorkhiyamonika@gmail.com>
2025-03-11 17:28:33 +0530

    fix: lighthouse changes

--- newapp/src/components/common/CaseStudyCarousel.svelte
+++ newapp/src/components/common/CaseStudyCarousel.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import * as Card from "$lib/components/ui/card/index.js";
   import * as Carousel from "$lib/components/ui/carousel/index.js";
-  import { BASE_URL } from "$lib/consts";
   import line from "$lib/images/line.svg";
+  export let baseUrl: string = "";
   export let casestudies: any[] = [];
 </script>
 
@@ -28,7 +28,7 @@
               >
               <a
                 target="_blank"
-                href={`${BASE_URL}/casestudies/${casestudy.data.attachments}`}
+                href={`${baseUrl}/casestudies/${casestudy.data.attachments}`}
                 aria-label="Learn more about ${casestudy.data.title}"
                 ><h2
                   class="mb-2 font-bold font-manrope text-[28px] leading-[34px] tracking-[0.02em] text-[#141414]"
