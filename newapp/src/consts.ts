0c98ba8a1dc949a30921082aacb33c8e03ce63a9
ridham-parmar <ridham.parmar@improwised.com>
2025-03-25 16:16:52 +0530

    fix: added LD Json

--- newapp/src/consts.ts
+++ newapp/src/consts.ts
@@ -6,3 +6,18 @@ export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const BASE_URL = import.meta.env.BASE_URL;
 export const SITE_KEY = import.meta.env.SITE_KEY;
 export const CONTACT_US_URL = import.meta.env.CONTACT_US_URL;
+export const LD_JSON_DATA = {
+    "@context": "https://schema.org",
+    "@type": "Corporation",
+    name: "Improwised Technologies Pvt. Ltd.",
+    alternateName: "Improwised Technologies",
+    url: "https://www.improwised.com/",
+    logo: "https://www.improwised.com/img/logo.png",
+    sameAs: [
+      "https://www.facebook.com/improwised",
+      "https://twitter.com/improwised",
+      "https://www.linkedin.com/company/improwised-technologies/",
+      "https://www.improwised.com/",
+      "https://github.com/Improwised/",
+    ],
+};
