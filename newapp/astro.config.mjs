3045d1870929274d2582bbefeb0f854bc8d4f961
ridham-parmar <ridham.parmar@improwised.com>
2025-04-23 20:10:32 +0530

    fix: removed base url

--- newapp/astro.config.mjs
+++ newapp/astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
   },
 
   // Subdirectory path
-  base: '/improwised-new-website/newapp',
+  // base: '/improwised-new-website/newapp',
 
   // Default output directory
   outDir: 'dist',
